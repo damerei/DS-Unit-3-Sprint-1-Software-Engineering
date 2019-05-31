6a01351f6902ca5f16435fc15e9d270c96157619
Jambavan <shantanu@olpci.org>
2019-05-31 17:41:36 -0400

    Adding .py files

---
+++ acme.py
@@ -0,0 +1,72 @@
+    '''
+    Initial set of classes for Acme products
+    '''
+
+from random import randint
+
+
+class Product:
+        '''
+        Initializer for creating Acme products
+        '''
+    def __init__(self, name, price=10, weight=20,
+                 flammability=.5, identifier=randint(1000000, 9999999)):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = identifier
+    
+    def stealability(self):
+            '''
+            calculating stealability as a function of price to weight
+            '''
+        steal_ratio = self.price / self.weight
+        if steal_ratio < .5:
+            return 'Not so stealable...'
+        elif steal_ratio < 1:
+            return 'Kinda stealable.'
+        else:
+            return 'Very stealable!'
+
+def explode(self):
+       '''
+        calculate how flammable or volatile a product is
+        '''
+            volatility = self.flammability * self.weight
+                if volatility < 10:
+                    return '...fizzle'
+                        elif volatility < 50:
+                            return '...boom!'
+                                else:
+                                    return '...BABOOM!!'
+
+
+class BoxingGlove(Product):
+        '''
+        Acme Boxing Glove is an instantiation of Proudct
+        '''
+    def __init__(self, name, price=10, weight=10,
+                 flammability=.5, identifier=randint(1000000, 9999999)):
+    
+            super().__init__(name=name, price=price, weight=weight,
+                     flammability=flammability,
+                     identifier=identifier)
+    
+    def explode(self):
+            '''
+            Overwriting the explode function, since gloves are not volatile or flammable.
+            '''
+        return "...it's a glove."
+    
+    def punch(self):
+            '''
+            Estimate punching power by glove weight
+            '''
+        if self.weight < 5:
+            return 'That tickles.'
+        elif self.weight < 15:
+            return 'Hey that hurt!'
+        else:
+            return 'OUCH!'
+
---
+++ acme_report.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+    '''
+    Methods for generating and reporting on lists of products
+    '''
+from random import randint, sample, uniform, choice
+from acme import Product
+
+
+def generate_products(quantity=30):
+        '''
+        Method for generating a gallimaufry of Acme products
+        '''
+    products = []
+    adjective = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
+    noun = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
+    
+    # create products with random values and append to products
+    for _ in range(quantity):
+        name = (choice(adjective) + ' ' + choice(noun))
+        price = randint(5, 100)
+        weight = randint(5, 100)
+        flammability = uniform(0, 2.5)
+        products.append(Product(name=name, price=price,
+                                weight=weight, flammability=flammability))
+    
+    return products
+
+
+def inventory_report(products):
+        '''
+        Method for giving a report on the inventory
+        '''
+    names = []
+    agg_price = 0
+    agg_weight = 0
+    agg_flammability = 0
+    product_count = len(products)
+    
+    # gather information for report
+    for product in products:
+        unique_names.append(product.name)
+        agg_price += product.price
+        agg_weight += product.weight
+        agg_flammability += product.flammability
+    
+    # print report to screen
+    print('ACME CORPORATION OFFICIAL INVENTORY REPORT')
+    print('Unique product names:', len(set(names)))
+    print('Average price:', (agg_price/product_count))
+    print('Average weight:', (agg_weight/product_count))
+    print('Average flammability:', (agg_flammability/product_count))
+
+
+if __name__ == '__main__':
+    inventory_report(generate_products())
